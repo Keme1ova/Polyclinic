29158ee386b90e07043669632775cebb835012a5
altynaykemelova <kemelovaaltynay24@gmail.com>
2023-06-17 14:57:00 +0600

    improve

--- style/js/js.js
+++ style/js/js.js
@@ -1,10 +1,19 @@
-function changeContent() {
-    var element = document.getElementById("myElement"); // Замените "myElement" на соответствующий идентификатор вашего элемента
-    
-    // Изменение содержимого элемента
-    element.innerHTML = "Другое содержимое"; // Замените "Другое содержимое" на желаемый текст или HTML-разметку
+function toggleDropdown() {
+  var dropdownContent = document.getElementById("dropdown-content");
+  dropdownContent.classList.toggle("show");
+}
+
+window.onclick = function(event) {
+  if (!event.target.matches('.dropdown-button')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    for (var i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
   }
-  
-  // Привязка функции к событию нажатия на иконку
-  var icon = document.getElementById("myIcon"); // Замените "myIcon" на соответствующий идентификатор вашей иконки
-  icon.addEventListener("click", changeContent);
+};
+
+
+
